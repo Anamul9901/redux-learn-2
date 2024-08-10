aebf71d288fcc03158abccae8cd65c1852665b7c
Anamul Haque <anamulhaque9901@gmail.com>
2024-08-10 15:47:41 +0600

    setup redux and create store file

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h3>Redux toolkit</h3>
+        <p>0</p>
+        <button>dis</button>
+        <button>inc</button>
+      </div>
+    </>
+  );
+}
+
+export default App;
